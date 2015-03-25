3b920b2ba33416e1dbb02b8ce3c95614e3190912
Simon Hafner <hafnersimon@gmail.com>
2015-03-26 04:02:18 +0500

    use NimNode instead of PNimrodNode

--- tests/macros/ttableandmacros.nim
+++ tests/macros/ttableandmacros.nim
@@ -1,8 +1,8 @@
 import macros, tables, hashes
 
-proc hash(v: PNimrodNode): THash = 4  # performance is for suckers
+proc hash(v: NimNode): THash = 4  # performance is for suckers
 macro test(body: stmt): stmt {.immediate.} =
-  var a = initCountTable[PNimrodNode]()
+  var a = initCountTable[NimNode]()
   a.inc(body)
   echo(a)
 
